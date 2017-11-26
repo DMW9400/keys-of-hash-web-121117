f4bd6d9d372d4289d26a64fc1ca2be1cd997e417
Matthew Welch <dmw9400@gmail.com>
2017-11-26 19:01:51 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -23,6 +23,7 @@ class Hash
   def keys_of(*arguments)
     return_array = []
     self.each do |key,value|
+      arguments.each do |item|
         if value == arguments
           return_array << key
         end
