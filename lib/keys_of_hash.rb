a62c90afbc8173932721a133373bc01ef6bc67a0
Matthew Welch <dmw9400@gmail.com>
2017-11-26 18:59:22 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -7,9 +7,20 @@
 #     "tomato frog"=>"Madagascar",
 #     "koala"=>"Australia"}
 #   }
+# class Hash
+#   def keys_of(arguments)
+#     return_array = []
+#     self.each do |key,value|
+#         if value == arguments
+#           return_array << key
+#         end
+#     end
+#       return return_array
+#   end
+# end
+
 class Hash
   def keys_of(arguments)
-
     return_array = []
     self.each do |key,value|
         if value == arguments
